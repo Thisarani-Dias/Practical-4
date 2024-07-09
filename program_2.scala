1e5c22e2889507a60a817bd5b5d3fe3ed0749f24
Thisarani Dias <140165499+Thisarani-Dias@users.noreply.github.com>
2024-07-08 20:06:29 -0700

    Update program_2.scala

--- program_2.scala
+++ program_2.scala
@@ -1,26 +1,17 @@
 package q2
 
+import scala.io.StdIn.readInt
+
 object program_2 {
-    def main(args: Array[String]): Unit = {
-      if (args.length != 1) {
-        println("Please provide exactly one integer argument.")
-        System.exit(1)
-      }
-
-      val input = try {
-        args(0).toInt
-      } catch {
-        case _: NumberFormatException =>
-          println("Please provide a valid integer.")
-          System.exit(1)
-          0     //program exits before reaching here
-      }
-
-      // Pattern matching according to input value
-      input match {
-        case x if x <= 0 => println("Negative/Zero is input")
-        case x if x % 2 == 0 => println("Even number is given")
-        case x if x % 2 != 0 => println("Odd number is given")
-      }
+  def main(args: Array[String]): Unit = {
+    println("Enter an integer: ")
+    val userInput = readInt()
+    // Pattern matching according to input value
+    userInput match {
+      case x if x <= 0 => println("Negative/Zero is input")
+      case x if x % 2 == 0 => println("Even number is given")
+      case x if x % 2 != 0 => println("Odd number is given")
     }
+  }
 }
+
